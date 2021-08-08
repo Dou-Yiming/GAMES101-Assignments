8be64f104636ae606f303fbcf1b8e2fbf02d005b
Iridoudou <2534936416@qq.com>
2021-08-08 17:26:10 +0800

    update code

--- Assignment7/code/Scene.cpp
+++ Assignment7/code/Scene.cpp
@@ -4,8 +4,8 @@
 
 #include "Scene.hpp"
 
-
-void Scene::buildBVH() {
+void Scene::buildBVH()
+{
     printf(" - Generating BVH...\n\n");
     this->bvh = new BVHAccel(objects, 1, BVHAccel::SplitMethod::NAIVE);
 }
@@ -18,17 +18,22 @@ Intersection Scene::intersect(const Ray &ray) const
 void Scene::sampleLight(Intersection &pos, float &pdf) const
 {
     float emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
+    for (uint32_t k = 0; k < objects.size(); ++k)
+    {
+        if (objects[k]->hasEmit())
+        {
             emit_area_sum += objects[k]->getArea();
         }
     }
     float p = get_random_float() * emit_area_sum;
     emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
+    for (uint32_t k = 0; k < objects.size(); ++k)
+    {
+        if (objects[k]->hasEmit())
+        {
             emit_area_sum += objects[k]->getArea();
-            if (p <= emit_area_sum){
+            if (p <= emit_area_sum)
+            {
                 objects[k]->Sample(pos, pdf);
                 break;
             }
@@ -37,22 +42,23 @@ void Scene::sampleLight(Intersection &pos, float &pdf) const
 }
 
 bool Scene::trace(
-        const Ray &ray,
-        const std::vector<Object*> &objects,
-        float &tNear, uint32_t &index, Object **hitObject)
+    const Ray &ray,
+    const std::vector<Object *> &objects,
+    float &tNear, uint32_t &index, Object **hitObject)
 {
     *hitObject = nullptr;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
+    for (uint32_t k = 0; k < objects.size(); ++k)
+    {
         float tNearK = kInfinity;
         uint32_t indexK;
         Vector2f uvK;
-        if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear) {
+        if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear)
+        {
             *hitObject = objects[k];
             tNear = tNearK;
             index = indexK;
         }
     }
-
 
     return (*hitObject != nullptr);
 }
@@ -63,50 +69,50 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
     // TO DO Implement Path Tracing Algorithm here
     Intersection inter = intersect(ray);
 
-    if(inter.happened)
+    if (inter.happened)
     {
-        if(inter.m->hasEmission())// hit source
-            return depth == 0 ? inter.m->getEmission() : Vector3f(0.,0.,0.);
+        if (inter.m->hasEmission()) // hit source
+            return depth == 0 ? inter.m->getEmission() : Vector3f(0., 0., 0.);
 
-        Vector3f L_dir(0.,0.,0.);
-        Vector3f L_indir(0.,0.,0.);
+        Vector3f L_dir(0., 0., 0.);
+        Vector3f L_indir(0., 0., 0.);
 
         Intersection light_inter;
-        float pdf_light=0.0f;
-        sampleLight(light_inter,pdf_light);
+        float pdf_light = 0.0f;
+        sampleLight(light_inter, pdf_light);
 
-        auto& N = inter.normal;
-        auto& NN = light_inter.normal;
+        auto &N = inter.normal;
+        auto &NN = light_inter.normal;
 
-        auto& obj_pos=inter.coords;
-        auto& light_pos=light_inter.coords;
+        auto &obj_pos = inter.coords;
+        auto &light_pos = light_inter.coords;
 
-        auto diff=light_pos-obj_pos;
-        auto light_dir=diff.normalized();
-        float dist_sqr=diff.x*diff.x+diff.y*diff.y+diff.z*diff.z;
+        auto diff = light_pos - obj_pos;
+        auto light_dir = diff.normalized();
+        float dist_sqr = diff.x * diff.x + diff.y * diff.y + diff.z * diff.z;
 
-        Ray light(obj_pos,light_dir);
-        Intersection light2obj=intersect(light);
+        Ray light(obj_pos, light_dir);
+        Intersection light2obj = intersect(light);
 
-        if(light2obj.happened && (light2obj.coords-light_pos).norm() < 1e-2)
+        if (light2obj.happened && (light2obj.coords - light_pos).norm() < 1e-2)
         {
             Vector3f f_r = inter.m->eval(ray.direction, light_dir, N);
-            L_dir = light_inter.emit * f_r * dotProduct(N,light_dir) * dotProduct(NN,-light_dir) / dist_sqr / pdf_light;
+            L_dir = light_inter.emit * f_r * dotProduct(N, light_dir) * dotProduct(NN, -light_dir) / dist_sqr / pdf_light;
         }
 
-        if(get_random_float()<RussianRoulette)
+        if (get_random_float() < RussianRoulette)
         {
-            Vector3f next_dir=inter.m->sample(ray.direction,N).normalized();
-            Ray next_ray(obj_pos,next_dir);
+            Vector3f next_dir = inter.m->sample(ray.direction, N).normalized();
+            Ray next_ray(obj_pos, next_dir);
             Intersection next_inter = intersect(next_ray);
-            if(next_inter.happened && !next_inter.m->hasEmission())
+            if (next_inter.happened && !next_inter.m->hasEmission())
             {
-                float pdf = inter.m->pdf(ray.direction,next_dir,N);
+                float pdf = inter.m->pdf(ray.direction, next_dir, N);
                 Vector3f f_r = inter.m->eval(ray.direction, next_dir, N);
-                L_indir = castRay(next_ray, depth+1) * f_r * dotProduct(N, next_dir) / pdf / RussianRoulette;
+                L_indir = castRay(next_ray, depth + 1) * f_r * dotProduct(N, next_dir) / pdf / RussianRoulette;
             }
         }
         return L_dir + L_indir;
     }
-    return Vector3f(0.,0.,0.);
+    return Vector3f(0., 0., 0.);
 }
